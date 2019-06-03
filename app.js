7b421ab998f1429ebbe38c575ec364c64a8df3ee
Tatjana <kuznetsovatatjana@gmail.com>
2019-06-03 08:32:21 +0300

    sending some new functionality with mistakes

--- app.js
+++ app.js
@@ -1,98 +1,70 @@
-//CANVAS//
-window.addEventListener('load', () => {
-    const canvas = document.querySelector("#canvas");
-    const clearAll = document.querySelector(".clean");
-    const ctx = canvas.getContext("2d");
-    //COLORS
-    const pink = document.querySelector(".pink");
-    const red = document.querySelector(".red");
-    const orange = document.querySelector(".orange");
-    const yellow = document.querySelector(".yellow");
-    const green = document.querySelector(".green");
-    const blue = document.querySelector(".blue");
-    const indigo = document.querySelector(".indigo");
-    const violet = document.querySelector(".violet");
-    const white = document.querySelector(".white");
-    const black = document.querySelector(".black");
+//Timer//
 
-    //variables
-    let painting = false;
-    let customLineWidth = document.querySelector("#linewidth");
-    let download = document.getElementById("download");
-    //functions
-    function startPosition(e){
-        painting = true;
-        draw();
-    }
-    function endPosition(){
-        painting = false;
-        ctx.beginPath();
-    }
-    function draw(e){
-        if(!painting) return;
-        ctx.lineWidth = customLineWidth.value;
-        ctx.lineCap = "round";
-        ctx.lineTo(e.clientX, e.clientY);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
-    }
-    function clearCanvas(){
-        ctx.clearRect(0,0, canvas.width, canvas.height);
-    }
-    function downloadImage(){
-        let image = document.getElementById("canvas").toDataURL("image/png")
-                    .replace("image/png", "image/octet-stream");
-        download.setAttribute("href", image);
-    }
-    //COLOR BUTTONS
-    function pinkCol(){
-        ctx.strokeStyle = pink.value;
-    }
-    function redCol(){
-        ctx.strokeStyle = red.value;
-    }
-    function orangeCol(){
-        ctx.strokeStyle = orange.value;
-    }
-    function yellowCol(){
-        ctx.strokeStyle = yellow.value;
-    }
-    function greenCol(){
-        ctx.strokeStyle = green.value;
-    }
-    function blueCol(){
-        ctx.strokeStyle = blue.value;
-    }
-    function indigoCol(){
-        ctx.strokeStyle = indigo.value;
-    }
-    function violetCol(){
-        ctx.strokeStyle = violet.value;
-    }
-    function whiteCol(){
-        ctx.strokeStyle = white.value;
-    }
-    function blackCol(){
-        ctx.strokeStyle = black.value;
-    }
-    //eventListeners
-    canvas.addEventListener('mousedown', startPosition);
-    canvas.addEventListener('mouseup', endPosition);
-    canvas.addEventListener('mousemove', draw);
-    clearAll.addEventListener('click', clearCanvas);
-    pink.addEventListener('click', pinkCol);
-    red.addEventListener('click', redCol);
-    orange.addEventListener('click', orangeCol);
-    yellow.addEventListener('click', yellowCol);
-    green.addEventListener('click', greenCol);
-    blue.addEventListener('click', blueCol);
-    indigo.addEventListener('click', indigoCol);
-    violet.addEventListener('click', violetCol);
-    white.addEventListener('click', whiteCol);
-    black.addEventListener('click', blackCol);
-    download.addEventListener('click', downloadImage);
-});
+window.onload = window.setTimeout("getSecs()", 1);
+startday = new Date();
+clockStart = startday.getTime();
+
+function initStopwatch() {
+  let myTime = new Date();
+  let timeNow = myTime.getTime();
+  let timeDiff = timeNow - clockStart;
+  this.diffSecs = timeDiff / 1000;
+  return this.diffSecs;
+}
+
+function getSecs() {
+  var mySecs = initStopwatch();
+  var mySecs1 = "" + mySecs;
+  mySecs1 = mySecs1.substring(0, mySecs1.indexOf(".")) + " seconds";
+  document.forms[0].timespent.value = mySecs1;
+  window.setTimeout("getSecs()", 1000);
+}
+
+//Accordeon//
+
+let title = document.getElementsByClassName('accordion-title');
+
+for (let i = 0; i < title.length; i++) {
+     title[i].addEventListener('click', function() {
+        if (!(this.classList.contains('active'))) {
+            for(let i = 0; i < title.length; i++) {
+                 title[i].classList.remove('active');
+             }
+            this.classList.add('active'); 
+        }
+    });
+}
+
+//Slider//
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
 
 //CONTACT FORM//
 
